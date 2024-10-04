64226719eb5c003f96c2918211e1ad9b2303d3a5
hurbx <hugo.urbina@mayor.cl>
2024-10-04 14:19:32 -0300

    cambios

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,9 +2,4 @@
 
 echo "Pull Finished"
 sudo systemctl daemon-reload
-if sudo systemctl restart nginx; then
-  echo "Nginx restarted successfully"
-else
-  echo "Failed to restart Nginx" >&2
-  sudo systemctl status nginx
-fi
+sudo systemctl restart nginx
