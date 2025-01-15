57c5154612969e01c4eb91aecadb6ab6a0fc8ba8
hurbx <hugo.urbina@mayor.cl>
2025-01-15 07:23:22 -0300

    creacion diccionario fusi

--- fusi/urls.py
+++ fusi/urls.py
@@ -1,4 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
 urlpatterns = [
+  path("diccionario-fusi/", views.dic_fusi, name='dic_fusi'),
   
-    
 ]
