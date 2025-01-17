62b2cf9a6c9f33a29817cda4575907fd884a7e24
hurbx <hugo.urbina@mayor.cl>
2025-01-17 11:15:58 -0300

    Revert "creacion diccionario fusi"
    This reverts commit 57c5154612969e01c4eb91aecadb6ab6a0fc8ba8.

--- fusi/urls.py
+++ fusi/urls.py
@@ -1,8 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
 urlpatterns = [
-  path("diccionario-fusi/", views.dic_fusi, name='dic_fusi'),
   
+    
 ]
--- fusi/views.py
+++ fusi/views.py
@@ -1,38 +1,3 @@
 from django.shortcuts import render
-from bus_signals.models import FusiMessage
-from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
-@login_required(login_url='login')
-def dic_fusi(request):
-    search_query = ''
-    if request.GET.get('search_query'):
-        search_query = request.GET.get('search_query')
-
-    messages = FusiMessage.fusi.filter(
-        Q(fusi_code__icontains=search_query) |
-        Q(fusi_description__icontains=search_query) |
-        Q(message_class__icontains=search_query)
-    )
-    # paginadorfusi
-    page = request.GET.get('page')
-    results = 17
-    paginator = Paginator(messages, results)
-    try:
-        messages = paginator.page(page)
-    except PageNotAnInteger:
-        page = 1
-        messages = paginator.page(page)
-    except EmptyPage:
-        page = paginator.num_pages
-        messages = paginator.page(page)
-    # fin paginador fusi
-
-    context = {
-        'fusi': messages,
-        'search_query': search_query,
-        'paginator': paginator
-    }
-    return render(request, 'fusi-dictionary.html', context)
