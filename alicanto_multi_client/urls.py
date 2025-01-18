93daa89e1eb76e488e64a59b7d8ebeade9430a67
hurbx <hugo.urbina@mayor.cl>
2025-01-18 14:30:09 -0300

    change config 2

--- alicanto_multi_client/urls.py
+++ alicanto_multi_client/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('energy.urls')),
@@ -25,3 +28,5 @@ urlpatterns = [
     path('', include('bus_signals.urls')),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
